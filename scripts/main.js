4b5d76301fdf4dbdb192c9c3a90558fab003d9ea
AngusParsons4909 <angus.parsons@mds.ac.nz>
2024-11-11 23:38:23 +1300

    changed the whole website lol
    changed the whole website lol

--- scripts/main.js
+++ scripts/main.js
@@ -1,43 +1,119 @@
-const myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello world!";
+const Score1 = document.querySelector(".Score1");
+const Score2 = document.querySelector(".Score2");
+const OutputText = document.querySelector(".TextWords");
+const OutputScore = document.querySelector(".TextScore");
+const Player1 = document.getElementById("Player1");
+const Player2 = document.getElementById("Player2");
+const EndScore = document.getElementById("Input1");
+const StartPlayer = document.getElementById("Input2");
 
-const myImage = document.querySelector("img");
+let Button1 = document.querySelector(".Player1Button");
+let Button2 = document.querySelector(".Player2Button");
+let ConfirmButton = document.querySelector(".ConfirmButton")
 
+Score1Value = 0;
+Score2Value = 0;
 
+CurrPoint = 0;
+PrevPoint = StartPlayer.selectedIndex;
+GamePoint = 0;
 
-myImage.onclick = () => {
-    const myScr = myImage.getAttribute("Src");
-    if (myScr === "images/firefox-icon.png") {
-        myImage.setAttribute("Src", "images/sonic.png")
-    }
-    else {
-        myImage.setAttribute("Src","images/firefox-icon.png")
+Button1.onclick = () => {
+    if (StartPlayer.selectedIndex !== 0 && EndScore.value !== '' && !isNaN(Number(EndScore.value)) && Number(EndScore.value) > 0 && OutputScore.textContent != "Error: [Incomplete setup]") {
+        if (Score1Value + 1 <= Number(EndScore.value) && Score2Value + 1 <= Number(EndScore.value)) {
+            if (Number(Score1Value) + 1 === Number(EndScore.value) || Score2Value + 1 === Number(EndScore.value)) {
+                if (Math.abs(Score1Value - Score2Value) >= 2 && (Score1Value === Number(EndScore.value) || Score2Value === Number(EndScore.value))) {
+                } 
+                if (Math.abs(Score1Value - Score2Value) === 0) {
+                    EndScore.value = Number(EndScore.value) + 1;
+                }
+            }
+            IncreaseScore1();
+            GeneralButtonFunc();
+        }
+        if (Number(Score1Value) === Number(EndScore.value) || Score2Value === Number(EndScore.value)) {
+            if (Score1Value > Score2Value){
+            OutputText.textContent = (Player1.value || Player1.placeholder) + " Wins!";
+            } else {
+                OutputText.textContent = (Player2.value || Player2.placeholder) + " Wins!";
+            }
+        }
     }
 };
 
-if (!localStorage.getItem("name")) {
-    setUserName();
-}
-else {
-    const storedName = localStorage.getItem("name");
-    myHeading.textContent = `${storedName} is epic`;
-}
-
-function setUserName() {
-    const myName = prompt("Please enter your name: ");
-    if (!myName) {
-        setUserName();
+Button2.onclick = () => {
+    if (StartPlayer.selectedIndex !== 0 && EndScore.value !== '' && !isNaN(Number(EndScore.value)) && Number(EndScore.value) > 0 && OutputScore.textContent != "Error: [Incomplete setup]") {
+        if (Score1Value + 1 <= Number(EndScore.value) && Score2Value + 1 <= Number(EndScore.value)) {
+            if (Number(Score1Value) + 1 === Number(EndScore.value) || Score2Value + 1 === Number(EndScore.value)) {
+                if (Math.abs(Score1Value - Score2Value) >= 2 && (Score1Value === Number(EndScore.value) || Score2Value === Number(EndScore.value))) {
+                } 
+                if (Math.abs(Score1Value - Score2Value) === 0) {
+                    EndScore.value = Number(EndScore.value) + 1;
+                }
+            }
+            IncreaseScore2();
+            GeneralButtonFunc();
+        }
+        if (Number(Score1Value) === Number(EndScore.value) || Score2Value === Number(EndScore.value)) {
+            if (Score1Value > Score2Value){
+            OutputText.textContent = (Player1.value || Player1.placeholder) + " Wins!";
+            } else {
+                OutputText.textContent = (Player2.value || Player2.placeholder) + " Wins!";
+            }
+        }
     }
-    else {
-        localStorage.setItem("name",myName);
-        myHeading.textContent = `${myName} is epic`;
+};
+
+ConfirmButton.onclick = () => {
+    if (Score1Value === 0 && Score2Value === 0) {
+        if (StartPlayer.selectedIndex === 1) {
+            OutputText.textContent = (Player1.value || Player1.placeholder) + " ball in hand, serve when ready."
+        } else if (StartPlayer.selectedIndex === 2) {
+            OutputText.textContent = (Player2.value || Player2.placeholder) + " ball in hand, serve when ready."
+        } else if (StartPlayer.selectedIndex === 0 || EndScore.value === '') {
+            OutputText.textContent = "Error: [Incomplete setup]"
+        }
     }
 }
-let myButton = document.querySelector("button");
 
-
-myButton.onclick = () => {
-    setUserName();
+function IncreaseScore1()
+{
+    if (PrevPoint === 0 && StartPlayer.selectedIndex === 2) {
+        OutputText.textContent = "Handout " + (Player1.value || Player1.placeholder);
+    }
+    Score1.textContent = Number(Score1.textContent) + 1;
+    CurrPoint = 1;
+    Score1Value +=1;
 }
 
+function IncreaseScore2() {
+    if (PrevPoint === 0 && StartPlayer.selectedIndex === 1) {
+        OutputText.textContent = "Handout " + (Player2.value || Player2.placeholder);
+    }
+    Score2.textContent = Number(Score2.textContent) + 1;
+    CurrPoint = 2;
+    Score2Value +=1;
+}
 
+function GeneralButtonFunc() {
+    if (PrevPoint === 1 && CurrPoint === 2) {
+        OutputText.textContent = "Handout " + (Player2.value || Player2.placeholder);
+    } else if (PrevPoint === 2 && CurrPoint === 1) {
+        OutputText.textContent = "Handout " + (Player1.value || Player1.placeholder);
+    } else {
+        OutputText.textContent = ""
+    }
+    if (CurrPoint === 1) {
+        OutputScore.textContent = Score1Value + " / " + Score2Value
+    } else if (CurrPoint === 2) {
+        OutputScore.textContent = Score2Value + " / " + Score1Value
+    }
+    if (Number(Score1Value) + 1 === Number(EndScore.value) || Number(Score2Value) + 1 === Number(EndScore.value)){
+        if (Score1Value === Score2Value) {
+            OutputScore.textContent = OutputScore.textContent + " win by two";
+        } else {
+            OutputScore.textContent = OutputScore.textContent + " game point";
+        }
+    }
+    PrevPoint = CurrPoint;
+}
